cb0d9257d1fb524a4ce485d4dfb445a26b23fc52
EnzoToniato567 <00001124789455sp@al.educacao.sp.gov.br>
2025-11-07 13:38:18 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
-const select = document.getElementById('material');
-const barreira = document.querySelector('.barreira');
-const raio = document.querySelector('.raio');
+const select = document.getElementById("material");
+const barreira = document.querySelector(".barreira");
+const raio = document.querySelector(".raio");
 
-select.addEventListener('change', () => {
+select.addEventListener("change", () => {
   const material = select.value;
   barreira.textContent = material;
 
-  if (material === 'papel') {
-    barreira.style.background = 'rgba(255, 215, 0, 0.1)';
-    raio.style.width = '300px';
-  } else if (material === 'aluminio') {
-    barreira.style.background = 'rgba(255, 215, 0, 0.4)';
-    raio.style.width = '180px';
+  if (material === "papel") {
+    barreira.style.background = "rgba(221, 8, 8, 0.1)";
+    raio.style.width = "300px";
+  } else if (material === "aluminio") {
+    barreira.style.background = "rgba(198, 94, 9, 0.4)";
+    raio.style.width = "180px";
   } else {
-    barreira.style.background = 'rgba(255, 215, 0, 0.8)';
-    raio.style.width = '80px';
+    barreira.style.background = "rgba(36, 213, 20, 0.8)";
+    raio.style.width = "80px";
   }
 });
